50e47f21082c86c7929b8f117ee747855d3563a7
Rohan <rohanchakraborty123@gmail.com>
2021-09-19 16:09:04 -0400

    adding foreign keys

--- sql/stock_schema.sql
+++ sql/stock_schema.sql
@@ -1,15 +1,30 @@
 create database if not exists stockdata; -- this is the main database
 use stockdata;
+
+create table if not exists tickers (
+	tickerid int primary key not null,
+	ticker varchar(50) not null
+);
+
+create table if not exists dates (
+	dateid int primary key not null,
+	datestring int not null
+);
+
 /*
 	The table holding the main stock information.
 */
 create table if not exists pricedata (
-	ticker varchar(50) primary key not null,
-	date int not null,
+	tickerid int primary key not null,
+	dateid int primary key not null,
 	openbid float not null,
 	openask float not null,
 	closebid float not null,
 	closeask float not null,
 	volume int not null,
-	some_more data
+	CONSTRAINT `fk_ticker`
+    	FOREIGN KEY (tickerid) REFERENCES tickers (tickerid),
+	CONSTRAINT `fk_date`
+    	FOREIGN KEY (dateid) REFERENCES tickers (dateid)
 );
+
